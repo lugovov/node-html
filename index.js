8582cac26ca6c5b4bb134101ad86401032d167fc
Alexander <git@lugovov.ru>
2017-12-02 17:54:25 +0300

    test module

---
+++ index.js
@@ -0,0 +1,22 @@
+var monoTags=['br,img,input'];
+var doubleTags=['a,p,span'];
+/** 
+* Формирует тег
+* @param {string} tag - TAG name
+* @param {array of objects} attrs - Attributes
+* @param {string|object|array of objects} elements - child Elements 
+* @return {string} 
+*/
+var createTag=function(tag,attrs,elements){
+
+}
+
+/** 
+* Формирует HTML
+* @param {object|array of objects} elements - child Elements 
+* @return {string} 
+*/
+
+module.exports.html=function(elements){
+    return 'test';
+}
